85e364019f3238ecb994ac003fc106829e8d721d
archan <joshua_0621@naver.com>
2021-02-22 16:37:54 +0900

    create member table

--- db.sql
+++ db.sql
@@ -2,5 +2,14 @@ create database coupang; --데이터베이스 생성
 
 use coupang; -- 데이터 베이스 사용
 
+create table member(        --회원 테이블 생성
+  id varchar(30) not null,
+  pw varchar(40),
+  email varchar(100),
+  date datetime,
+  phone varchar(15),
+    constraint id_pk primary key (id)
+);
+
 
 
